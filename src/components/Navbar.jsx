b6037cd6d5f6f3c15fb78c6369416fdc976b5751
JuanmisDev <lopezjuanmiguel37@gmail.com>
2024-02-27 20:07:59 -0500

    Services section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-orange-400 text-gray-200">
           {links.map(({ id, link, name }) => (
             <li
               key={id}

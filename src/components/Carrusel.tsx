f0dc1566f52c78cae6f0ee0cbe51697fecf84673
JuanmisDev <lopezjuanmiguel37@gmail.com>
2024-03-03 13:07:49 -0500

    reponsiveness about section

--- src/components/Carrusel.tsx
+++ src/components/Carrusel.tsx
@@ -8,7 +8,11 @@ function Carrusel() {
         {
             original: 'https://picsum.photos/id/1018/1000/600/',
             thumbnail: 'https://picsum.photos/id/1018/250/150/',
-        }
+        },
+        {
+          original: 'https://picsum.photos/id/1018/1000/600/',
+          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+      }
     ]
 
   return (
